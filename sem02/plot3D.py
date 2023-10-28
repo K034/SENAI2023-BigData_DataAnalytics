f627d3bf41def32bb2dc0399f7545ab2235a9da9
_KOBA_ <79914307+K034@users.noreply.github.com>
2023-10-28 15:41:40 -0300

    Add files via upload

---
+++ sem02/plot3D.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+alpha = 0.7
+phi_ext = 2*np.pi*0.5
+
+def ColorMap(phi_m, phi_p):
+    return (+ alpha -2 * np.cos(phi_p) * np.cos(phi_m) - alpha * np.cos(phi_ext -2*phi_p) )
+
+phi_m = np.linspace(0, 2*np.pi, 100)
+phi_p = np.linspace(0, 2*np.pi, 100)
+X,Y = np.meshgrid(phi_p, phi_m)
+Z = ColorMap(X,Y).T
+
+fig = plt.figure( figsize =(8,6))
+
+ax = fig.add_subplot(1,1,1, projection ='3d')
+
+p =ax.plot_wireframe(X, Y, Z, rstride=4, cstride=4)
---
+++ sem02/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2*np.pi*t)
+
+fig,ax =plt.subplots()
+ax.plot(t,s)
+
+plt.savefig('ex_plot.png', format='png')
